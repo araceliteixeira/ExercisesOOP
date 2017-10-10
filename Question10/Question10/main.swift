14ad9cac3eec44b155491cf525d70c2d97573abb
Araceli Teixeira <araceliteixeira@Franciscos-MacBook-Air.local>
2017-10-10 13:45:59 -0400

    Update

--- Question10/Question10/main.swift
+++ Question10/Question10/main.swift
@@ -2,8 +2,8 @@
 //  main.swift
 //  Question10
 //
-//  Created by Francisco Freitas on 2017-10-10.
-//  Copyright © 2017 FranciscoFreitas. All rights reserved.
+//  Created by Araceli Teixeira on 2017-10-10.
+//  Copyright © 2017 Araceli Teixeira. All rights reserved.
 //
 
 import Foundation
--- Question11/Question11/main.swift
+++ Question11/Question11/main.swift
@@ -2,8 +2,8 @@
 //  main.swift
 //  Question11
 //
-//  Created by Francisco Freitas on 2017-10-10.
-//  Copyright © 2017 FranciscoFreitas. All rights reserved.
+//  Created by Araceli Teixeira on 10/10/17.
+//  Copyright © 2017 Araceli Teixeira. All rights reserved.
 //
 
 import Foundation
